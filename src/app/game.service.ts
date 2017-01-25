5233ca6f5948fa5144e58ecf5e76529ba3701d55
Bryant Wang and Kevin Finley <bryant@>
2017-01-25 10:05:47 -0800

    Add content to game service, specifically properties, constructor that fills out the board, getRandom and placeMines methods.

--- src/app/game.service.ts
+++ src/app/game.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { Tile } from './tile.model';
 
 @Injectable()
 export class GameService {
-  board = [];
+  public board;
+  public time: number;
+  public clickNumber: number;
 
-  constructor() { }
+  constructor(public mineNumber: number, public xSize: number, public ySize: number) {
+    this.board = new Array(ySize);
+    for(var y=0;y < ySize;y++) {
+      for(var x=0;x< xSize;x++) {
+        this.board[y].push(new Tile(y, x));
+      }
+    }
+  }
+
+  getRandom(size: number) {
+    return Math.floor(Math.random() * (size + 1));
+  }
+
+  placeMines() {
+    for(var i=0;i < this.mineNumber;i++) {
+      var randomY: number = this.getRandom(this.ySize);
+      var randomX: number = this.getRandom(this.xSize);
+
+      if(this.board[randomY][randomX].mineHere === false) {
+        this.board[randomY][randomX].mineHere = true;
+      } else {
+        i--;
+      }
+    }
+
+  }
 
 }
